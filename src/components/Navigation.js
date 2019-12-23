379906e6d764dc51e56f7ffa41de18faa298731a
LeedWover <aresone.gaben@gmail.com>
2019-12-23 23:29:20 +0100

    ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Icon from '../assets/starwars2.png';
+import Icon from "../assets/starwars3.png";
 
 function Navigation() {
   return (
-    <nav>
-      <div className="nav-wrapper grey darken-4">
-        <a href="#" className="brand-logo">
-          Star
-          <img src={Icon} style={{height: '2em'}} />
-          Wars
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down" style={{fontSize: '3em'}}>
-          <li>
-            <Link to="/" style={{fontSize: '0.5em'}}>Home</Link>
-          </li>
-          <li>
-            <Link to="/characters" style={{fontSize: '0.5em'}}>Characters</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav>
+        <div className="nav-wrapper grey darken-4">
+          <Link
+            to="/"
+            className="brand-logo"
+            style={{ padding: "0.15em 0 0 0.7em" }}
+          >
+            <img src={Icon} style={{ height: "1em" }} />
+          </Link>
+
+          <a href="#" data-target="mobile" className="sidenav-trigger">
+            <i style={{ fontSize: "2.4em", color: "#E6AB03" }} className="fas fa-bars"></i>
+          </a>
+          <ul
+            id="nav-mobile"
+            className="right hide-on-med-and-down Custom-font"
+          >
+            <li>
+              <Link to="/" style={{ fontSize: "2.4em", color: "#E6AB03" }}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/characters"
+                style={{ fontSize: "2.4em", color: "#E6AB03" }}
+              >
+                Characters
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <ul className="sidenav" id="mobile">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/characters">Characters</Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
